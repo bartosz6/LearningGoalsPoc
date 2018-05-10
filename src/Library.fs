ee3a30fc9fa5e56c959c1fdf3b260d81825eb65a
Bartosz Kowalczyk <kontakt@feree.pl>
2018-05-10 17:53:39 +0200

    rename proj

--- src/Library.fs
+++ src/Library.fs
@@ -1,5 +1,11 @@
-namespace src
+namespace Domain
+    module Goal =
+        type T = {
+            SubGoals: Goal list;
+            Progress: Progress;
+        }
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+        type Progress = private Progress of int
+        module Progress =
+            let create value = value
+            let value progress = progress
